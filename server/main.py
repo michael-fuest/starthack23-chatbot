f11a9f8de3f9e5930e2ba10bfd2df950586a2bdb
Michael Fuest <ge69fuh@mytum.de>
2023-03-23 21:46:23 +0100

    Moved some variables around

--- server/main.py
+++ server/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
 from typing import Dict, Callable
 from deepgram import Deepgram
 from dotenv import load_dotenv
@@ -10,9 +9,25 @@ import openai
 import os
 import pyttsx3
 
+clear_buffer = False
+
+def change_voice(engine, language, gender='VoiceGenderFemale'):
+    for voice in engine.getProperty('voices'):
+        if language in voice.languages and gender == voice.gender:
+            engine.setProperty('voice', voice.id)
+            return True
+
+    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name='static')
+engine = pyttsx3.init()
+try:
+     engine.setProperty('voice', 'com.apple.speech.synthesis.voice.yannick.premium')
+except:
+    change_voice(engine, 'de_DE', 'VoiceGenderMale')
+engine.setProperty('rate', 130)
+engine.setProperty('volume', 10)
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -38,32 +53,15 @@ async def process_audio(fast_socket: WebSocket):
 
 
 def process_response(prompt: str):
-    global is_talking
     response = get_response(prompt)
-    is_talking = True
     play_response(response)
-    is_talking = False
     return response
 
-
-def change_voice(engine, language, gender='VoiceGenderFemale'):
-    for voice in engine.getProperty('voices'):
-        if language in voice.languages and gender == voice.gender:
-            engine.setProperty('voice', voice.id)
-            return True
-
-    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
-
 def play_response(text):
-    engine = pyttsx3.init()
-    try:
-        engine.setProperty('voice', 'com.apple.speech.synthesis.voice.yannick.premium')
-    except:
-        change_voice(engine, 'de_DE', 'VoiceGenderMale')
-    engine.setProperty('rate', 130)
-    engine.setProperty('volume', 10)
+    global clear_buffer
     engine.say(text)
     engine.runAndWait()
+    clear_buffer = True
 
 async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
     try:
@@ -104,6 +102,7 @@ def get(request: Request):
 
 @app.websocket("/listen")
 async def websocket_endpoint(websocket: WebSocket):
+    global clear_buffer
     await websocket.accept()
 
     try:
@@ -111,6 +110,11 @@ async def websocket_endpoint(websocket: WebSocket):
 
         while True:
             data = await websocket.receive_bytes()
+
+            if clear_buffer:
+                websocket.clear_buffer()
+                clear_buffer = False
+
             deepgram_socket.send(data)
     except Exception as e:
         raise Exception(f'Could not process audio: {e}')
